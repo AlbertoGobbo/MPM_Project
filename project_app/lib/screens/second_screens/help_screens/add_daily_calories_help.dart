83d1046e9b59016236294181ec68843a19075c60
AlbertoGobbo <47864124+AlbertoGobbo@users.noreply.github.com>
2022-07-03 17:38:25 +0200

    Add add_daily_calories page

--- project_app/lib/screens/second_screens/help_screens/add_daily_calories_help.dart
+++ project_app/lib/screens/second_screens/help_screens/add_daily_calories_help.dart
@@ -18,7 +18,7 @@ class AddDailyCaloriesHelp extends StatelessWidget {
               // TEXT
               alignment: Alignment.centerLeft,
               child: Text(
-                "- Tap on the circular slider box.",
+                "- Tap on the circular slider box located in the homepage.",
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
@@ -27,7 +27,24 @@ class AddDailyCaloriesHelp extends StatelessWidget {
               // IMAGE
               alignment: Alignment.bottomCenter,
               child: Image.asset(
-                "assets/create_recipe/create_recipe_1.gif",
+                "assets/add_daily_calories/add_daily_calories_1.jpg",
+                scale: 4,
+              ),
+            ),
+            const SizedBox(height: 40),
+            const Align(
+              // TEXT
+              alignment: Alignment.centerLeft,
+              child: Text(
+                  "- If you are sure about your calories goal, enter the number of calories on the text field and tap the 'Set Calories' button.",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ),
+            const SizedBox(height: 20),
+            Align(
+              // IMAGE
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                "assets/add_daily_calories/add_daily_calories_2.gif",
                 scale: 1.5,
               ),
             ),
@@ -36,24 +53,7 @@ class AddDailyCaloriesHelp extends StatelessWidget {
               // TEXT
               alignment: Alignment.centerLeft,
               child: Text(
-                  "- If you are sure about your calories target, enter the number of calories on the text field and tap the 'Set Calories' button.",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            ),
-            const SizedBox(height: 20),
-            Align(
-              // IMAGE
-              alignment: Alignment.bottomCenter,
-              child: Image.asset(
-                "assets/create_recipe/create_recipe_2.jpg",
-                scale: 2,
-              ),
-            ),
-            const SizedBox(height: 40),
-            const Align(
-              // TEXT
-              alignment: Alignment.centerLeft,
-              child: Text(
-                  "- Otherwise, your application can help you to set your goal inserting some information about you. Indeed, you need to insert your age, weight and height on the text fields and tap on the other available options. Tapping on the 'Calculate Calories' button, the suggested calories are given, and you can set them tapping on 'Set Goal' or turn back to the data tapping on 'Recalculate'.",
+                  "- Otherwise, we can help you to set your calories goal inserting some information about you, as: age, weight, height, the daily activity level and workout during the week. Tapping on the 'Calculate Calories' button, the suggested calories are given, and you can set them tapping on 'Set Goal' or turn back to the data tapping on 'Recalculate'.",
                   textAlign: TextAlign.left,
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
             ),
@@ -62,7 +62,7 @@ class AddDailyCaloriesHelp extends StatelessWidget {
               // IMAGE
               alignment: Alignment.bottomCenter,
               child: Image.asset(
-                "assets/create_recipe/create_recipe_3.gif",
+                "assets/add_daily_calories/add_daily_calories_3.gif",
                 scale: 1.5,
               ),
             ),
@@ -71,7 +71,7 @@ class AddDailyCaloriesHelp extends StatelessWidget {
               // TEXT
               alignment: Alignment.centerLeft,
               child: Text(
-                  "Once you set the calories goal, the circular slider box in the homepage is updated.",
+                  "Once you set the calories goal, the circular slider box is updated.",
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
             ),
             const SizedBox(height: 20),
